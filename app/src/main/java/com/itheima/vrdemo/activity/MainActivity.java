9021bcb460ba704c69719913d9cb8937300b3702
Leon <526247082@qq.com>
2017-02-17 11:07:01 +0800

    Delete useless files.

--- app/src/main/java/com/itheima/vrdemo/activity/MainActivity.java
+++ app/src/main/java/com/itheima/vrdemo/activity/MainActivity.java
@@ -1,55 +1,15 @@
 package com.itheima.vrdemo.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.itheima.vrdemo.R;
-import com.itheima.vrdemo.fragment.PanoramaListFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TabLayout mTabLayout;
-    private ViewPager mViewPager;
-
-    private static final String[] TITLES = {"全景图", "360视频"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        mViewPager = (ViewPager) findViewById(R.id.view_pager);
-
-        mViewPager.setAdapter(mFragmentPagerAdapter);
-        mTabLayout.setupWithViewPager(mViewPager);
     }
-
-    private FragmentPagerAdapter mFragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-                    return new PanoramaListFragment();
-                case 1:
-                    return new Fragment();
-            }
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            return 2;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return TITLES[position];
-        }
-    };
 }
